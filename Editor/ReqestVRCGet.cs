694c3deddadcc04961e2875c9ba73480c9a3a496
Reina_Sakiria <sasiki.64892@gmail.com>
2024-02-15 13:52:37 +0900

    init

---
+++ Editor/ReqestVRCGet.cs
@@ -0,0 +1,133 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+namespace net.rs64.VRCGetForUnityEditor
+{
+    internal static class RequestVRCGet
+    {
+        private static ProcessStartInfo VRCGetStartInfo(string arguments)
+        {
+            var startInfo = new ProcessStartInfo();
+            startInfo.WorkingDirectory = Environment.CurrentDirectory;
+            startInfo.RedirectStandardError = true;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.UseShellExecute = false;
+            startInfo.CreateNoWindow = true;
+            // startInfo.StandardOutputEncoding = startInfo.StandardErrorEncoding = startInfo.StandardOutputEncoding = Encoding.GetEncoding("shift_jis");
+            startInfo.FileName = "vrc-get";
+            startInfo.Arguments = arguments;
+
+            return startInfo;
+        }
+
+        private static string Request(string arguments, int wait = 100)
+        {
+            using (var proses = new Process())
+            {
+                proses.StartInfo = VRCGetStartInfo(arguments);
+                proses.Start();
+
+                proses.WaitForExit(wait);
+                // if (!proses.HasExited) { proses.Kill(); return null; }
+                // try { if (proses.ExitCode != 0) { return proses.StandardError.ReadToEnd(); } } catch (Exception e) { Debug.LogException(e); }
+
+                return proses.StandardOutput.ReadToEnd();
+            }
+        }
+        private static async void RequestVoid(string arguments)
+        {
+            await Task.Run(() => runProses(arguments));
+
+            static void runProses(string arguments)
+            {
+                using (var proses = new Process())
+                {
+                    proses.StartInfo = VRCGetStartInfo(arguments);
+                    proses.Start();
+
+                    proses.WaitForExit();
+
+                    try { if (proses.ExitCode != 0) { Debug.LogError(proses.StandardError.ReadToEnd()); return; } } catch (Exception e) { Debug.LogException(e); }
+
+                    Debug.Log(proses.StandardOutput.ReadToEnd());
+                }
+            }
+        }
+        public static Project GetPackages()
+        {
+            return JsonUtility.FromJson<Project>(Request("info project --json-format 1"));
+        }
+
+        public static List<string> GetVersions(string packageName)
+        {
+            return JsonUtility.FromJson<OutputVersions>(Request($"info package {packageName} --json-format 1"))?.versions?.Select(Selector)?.ToList();
+            static string Selector(OutputVersions.OutputVersion i) => i.version;
+        }
+        public static void Install(string packageName)
+        {
+            RequestVoid($"install --yes {packageName}");
+        }
+        public static void Install(string packageName, string version)
+        {
+            RequestVoid($"install --yes {packageName} {version}");
+        }
+        public static string Remove(string packageName)
+        {
+            return Request($"remove {packageName}");
+        }
+
+        public static void Upgrade()
+        {
+             RequestVoid("upgrade");
+        }
+
+        public static List<string> Repositories()//URLが返ってくる
+        {
+            return Request("repo list").Split("\n").Select(i =>
+            {
+                var s = i.Split(" ");
+                if (s.Length < 3) { return null; }
+                return s[s.Length - 3];
+            }).Where(i => i != null).ToList();
+        }
+        public static List<string> PackageNames(string urlOrName)
+        {
+            var result = Request($"repo packages {urlOrName}");
+            if (result == null) { return null; }
+            return result.Split("\n").Where(i => !string.IsNullOrWhiteSpace(i) && i.Contains("| ")).Select(i => i.Split("| ")?.LastOrDefault()).Where(i => !string.IsNullOrWhiteSpace(i)).ToList();
+        }
+
+        [Serializable]
+        public class OutputVersions
+        {
+            public OutputVersion[] versions;
+            [Serializable]
+            public class OutputVersion
+            {
+                public string version;
+            }
+        }
+    }
+
+    [Serializable]
+    internal class Project
+    {
+        public string unity_version;
+        public Package[] packages;
+    }
+
+    [Serializable]
+    public class Package
+    {
+        public string name;
+        public string installed;
+        public string locked;
+        //public ??? requested;
+    }
+}
---
+++ Editor/VRCGetForUnityEditor.cs
@@ -0,0 +1,135 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UIElements;
+using System;
+
+namespace net.rs64.VRCGetForUnityEditor
+{
+
+    internal class VRCGetForUnityEditor : EditorWindow
+    {
+        [SerializeField]
+        private StyleSheet uss;
+
+        [MenuItem("Tools/VRCGetForUnityEditor")]
+        private static void ShowWindow()
+        {
+            var window = GetWindow<VRCGetForUnityEditor>();
+            window.titleContent = new GUIContent("VRCGetForUnityEditor");
+            window.Show();
+        }
+
+        public void CreateGUI()
+        {
+            rootVisualElement.Clear();
+
+            uss ??= AssetDatabase.LoadAssetAtPath<StyleSheet>(AssetDatabase.GUIDToAssetPath("5ca71c7c917a6d2449e4157f103d7549"));
+            if (uss != null) { rootVisualElement.styleSheets.Add(uss); }
+            else { Debug.LogWarning("スタイルが存在しません"); }
+
+            var project = RequestVRCGet.GetPackages();
+
+            var reDrawButton = new Button(CreateGUI);
+            reDrawButton.text = "Refresh";
+            rootVisualElement.Add(reDrawButton);
+
+            rootVisualElement.Add(new Label($"ThisProjectVersion : {project.unity_version}"));
+
+            var scrollView = new ScrollView();
+            var content = scrollView.Q<VisualElement>("unity-content-container");
+
+            rootVisualElement.Add(scrollView);
+
+            var upgradeButton = new Button(RequestVRCGet.Upgrade);
+            upgradeButton.text = "Upgrade";
+            content.hierarchy.Add(upgradeButton);
+
+            content.hierarchy.Add(new Label("Packages---------"));
+
+            var packageContainer = new VisualElement();
+            content.hierarchy.Add(packageContainer);
+
+            foreach (var package in project.packages)
+            {
+                var packageVi = new VisualElement();
+                packageVi.AddToClassList("PackageContainer");
+
+                packageVi.hierarchy.Add(new Label(package.name));
+
+                var rightElement = new VisualElement();
+                rightElement.AddToClassList("PackageContainerRight");
+                packageVi.hierarchy.Add(rightElement);
+                if (!TryCreateVersionSelector(package, rightElement)) { rightElement.hierarchy.Add(new Label(package.installed)); }
+
+                var removeButton = new Button(() => { Debug.Log(RequestVRCGet.Remove(package.name)); });
+                removeButton.text = "Remove";
+
+                rightElement.hierarchy.Add(removeButton);
+
+                packageContainer.hierarchy.Add(packageVi);
+            }
+
+            static bool TryCreateVersionSelector(Package package, VisualElement packageVi)
+            {
+                if (string.IsNullOrWhiteSpace(package.locked)) { return false; }
+
+                var versions = RequestVRCGet.GetVersions(package.name);
+                if (versions == null) { return false; }
+
+                var popup = new PopupField<string>(versions, package.installed);
+                popup.RegisterValueChangedCallback(v => RequestVRCGet.Install(package.name, v.newValue));
+                packageVi.hierarchy.Add(popup);
+                return true;
+            }
+            var addPackagesContainer = new VisualElement();
+            var addWindowButton = new Button(() => ShowAddPackages(addPackagesContainer));
+            addWindowButton.text = "ShowAddPackages";
+            content.hierarchy.Add(addWindowButton);
+            content.hierarchy.Add(addPackagesContainer);
+
+            static void ShowAddPackages(VisualElement root)
+            {
+                root.hierarchy.Clear();
+                foreach (var url in RequestVRCGet.Repositories())
+                {
+                    var container = new Foldout();
+                    container.text = url;
+                    container.value = false;
+                    var foldingContainer = container.Q("unity-content");
+                    try
+                    {
+                        var names = RequestVRCGet.PackageNames(url);
+                        if (names == null) { continue; }
+                        foreach (var packageName in names)
+                        {
+                            var packageContainer = new VisualElement();
+                            packageContainer.AddToClassList("PackageContainer");
+
+                            packageContainer.hierarchy.Add(new Label(packageName));
+                            var addButton = new Button(() => RequestVRCGet.Install(packageName));
+                            addButton.text = "Add";
+                            packageContainer.hierarchy.Add(addButton);
+
+                            foldingContainer.hierarchy.Add(packageContainer);
+                        }
+                        root.hierarchy.Add(container);
+                    }
+                    catch (Exception e)
+                    {
+                        Debug.LogError(e);
+                        Debug.LogError(url);
+                    }
+                }
+
+            }
+
+
+
+
+
+        }
+
+    }
+}
